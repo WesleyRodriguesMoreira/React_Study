e9fbec39f7e6320da5e6589ccf5f43972fa09f40
Wesley Rodrigues <wesleyrodriguesmoreira02@gmail.com>
2024-09-21 14:31:09 -0300

    3° commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import './App.css';
+import { useState } from 'react';
+import AddTasks from './components/AddTasks';
 import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Estuda",
+      description: "Olá Mundo",
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      text: "Traalha",
+      description: "Olá Mundo",
+      isCompleted: true,
+    },
+    {
+      id: 3,
+      text: "Descansa",
+      description: "Olá Mundo",
+      isCompleted: true,
+    },
+  ]);
 
   return (
-    <>
-      <Tasks />
-    </>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+        <div className="w-[500px]">
+            <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciado de Tarefas</h1>
+            <AddTasks/>
+            <Tasks tasks={tasks}/>
+        </div>
+
+    </div>
   )
 }
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,5 @@
-function Tasks() {
+function Tasks(props) {
+  console.log(props)
   return (
     <div>
       <h1>Gerenciado de Tarefas</h1>
